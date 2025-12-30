e831d446e5b1caa7d2ebfb7a6130b6f24eb42ddc
محمد میثم کشوری <15766@refahbank.net>
2025-12-31 01:17:44 +0330

    2025-12-31

---
+++ SnippetCode/intro.go
@@ -0,0 +1,51 @@
+package main	
+
+import "fmt"
+
+
+func main(){
+	sliceShareData()
+	fmt.Println(sum(1,2,3))
+	fmt.Println(sum(1,2,3,1))
+
+	greet("Hello","Alice","Bob","john")
+
+	fmt.Println(multiplyAll(5,2))
+	fmt.Println(multiplyAll(5,3))
+	fmt.Println(multiplyAll())
+}
+
+func sliceShareData(){
+	a := []int {1,2,3}
+	b := a 
+	b[1] = 99
+	fmt.Println(a)
+}
+
+func sum(nums ...int)int {
+	total:=0
+	for _,n := range nums {
+		total += n 
+	} 
+	return total
+}
+
+func greet(prefix string,names ...string){
+
+	for _,name := range names {
+		fmt.Println(prefix,name)
+	}
+}
+
+func multiplyAll(nums ... int) int {
+
+	if len(nums) == 0 {
+		return 1 
+	}
+
+	result := 1 
+	for _,n := range nums{
+		result *= n
+	}
+	return result
+}
