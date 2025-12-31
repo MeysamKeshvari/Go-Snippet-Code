aedd0acc9bb94c62150b480c228fe33951aeccd5
محمد میثم کشوری <15766@refahbank.net>
2025-12-31 13:36:09 +0330

    2025-12-31

--- SnippetCode/intro1.go
+++ SnippetCode/intro1.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main(){
+	showDetail(pizza)
 	
+}
+
+func pizza()(string , int ,string){
+	return "PiZZA" , 3500 , "Cheese + PePeroni"
+}
+
+func showDetail(f func() (string,int,string)){
+	name,price,ingredients := f()
+	fmt.Println("Show food details:")
+	fmt.Println("Name:" , name)
+	fmt.Println("Price:" , price)
+	fmt.Println("ingredients:" , ingredients)
 }
