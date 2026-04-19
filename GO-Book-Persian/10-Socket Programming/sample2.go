071ba69fb22ebcae02ad6e75079f99a29420e5ca
MeysamKeshvari <15766@refahbank.net>
2026-04-19 17:21:38 +0330

    2026-04-19

---
+++ GO-Book-Persian/10-Socket Programming/sample2.go
@@ -0,0 +1,36 @@
+package main
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func main(){
+
+	conn, err := net.Dial("tcp","localhost:5060")
+
+	if err!=nil {
+		panic("error establishing connection, make sure the server is up")
+	}
+
+	defer conn.Close()
+
+	log.Println("Connected to a tcp server on localhost:5060")
+	fmt.Println("Write something to send to server")
+
+	var message string
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print(">> ")
+		scanner.Scan()
+		message = scanner.Text()
+		if message == "exit" {
+			return
+		}
+
+		fmt.Fprint(conn,message)
+	}
+}
